acf35d5cfc8a890baed4873591ab38b1b198542f
Funergy <niels.dewulf48@gmail.com>
2014-09-28 15:20:30 +0200

    Created the package + main class

---
+++ BedWars/src/com/funergy/bedwars/Bedwars.java
@@ -0,0 +1,25 @@
+/*******************************************************************
+ * Copyright (c) 2014 Soulpoint Company
+ * THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS 
+ * ECLIPSE PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR 
+ * DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENT’S ACCEPTANCE
+ * OF THIS AGREEMENT. The full license is available at:
+ * http://www.eclipse.org/org/documents/epl-v10.php
+ ******************************************************************/
+package com.funergy.bedwars;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Funergy
+ *
+ */
+public class Bedwars extends JavaPlugin{
+	
+	public void onEnable() {
+		/*
+		 * Not started this project yet!
+		 */
+	}
+
+}
