6edb4ab4e9ecc009e1846f9a71c5df594eb960da
Funergy <niels.dewulf48@gmail.com>
2014-09-28 18:05:38 +0200

    Added CategoryChooser for shops

--- BedWars/src/com/funergy/bedwars/Bedwars.java
+++ BedWars/src/com/funergy/bedwars/Bedwars.java
@@ -8,11 +8,13 @@
  ******************************************************************/
 package com.funergy.bedwars;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.funergy.bedwars.categorychooser.ShopCategories;
 import com.funergy.bedwars.shop.Armor;
 
 /**
@@ -22,6 +24,7 @@ import com.funergy.bedwars.shop.Armor;
 public class Bedwars extends JavaPlugin{
 	 public static Bedwars instance;
 	public void onEnable() {
+		Bukkit.getPluginManager().registerEvents(new ShopCategories(), this);
 		instance = this;
 	}
 	
