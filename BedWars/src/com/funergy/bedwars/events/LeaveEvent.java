af6a2445aaa3733ca13cfa055e152978d853e86a
Funergy <niels.dewulf48@gmail.com>
2014-10-01 15:19:15 +0200

    Added leave event

---
+++ BedWars/src/com/funergy/bedwars/events/LeaveEvent.java
@@ -0,0 +1,36 @@
+/*******************************************************************
+ * Copyright (c) 2014 Soulpoint Company
+ * THE ACCOMPANYING PROGRAM IS PROVIDED UNDER THE TERMS OF THIS 
+ * ECLIPSE PUBLIC LICENSE ("AGREEMENT"). ANY USE, REPRODUCTION OR 
+ * DISTRIBUTION OF THE PROGRAM CONSTITUTES RECIPIENT’S ACCEPTANCE
+ * OF THIS AGREEMENT. The full license is available at:
+ * http://www.eclipse.org/org/documents/epl-v10.php
+ ******************************************************************/
+package com.funergy.bedwars.events;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.ItemStack;
+
+import com.funergy.bedwars.Bedwars;
+import com.funergy.bedwars.gamemanager.InGameHandler;
+import com.funergy.bedwars.gamemanager.Teams;
+
+/**
+ * @author Funergy
+ *
+ */
+public class LeaveEvent implements Listener{
+	@EventHandler
+	public void onLeaveEvent(PlayerQuitEvent e){
+		if(Bedwars.getGameState().equalsIgnoreCase("lobby")){
+			if(InGameHandler.getTeam(e.getPlayer())!=null){
+			Teams.removePlayerFromTeam(e.getPlayer());
+			e.getPlayer().getInventory().clear();
+		    e.getPlayer().getInventory().setArmorContents(new ItemStack[4]);
+			}
+		}
+	}
+
+}
